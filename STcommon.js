f36e9d47c8a968b18fdfad7d86ee5063c2fb4674
Joe Philipps <gituser@joe.philipps.us>
2017-06-30 20:44:53 -0400

    minor tweaks to functions, add comments for documentation

--- STcommon.js
+++ STcommon.js
@@ -16,13 +16,13 @@ function get_show_start_hour_offset() {
     // computer's configured local timezone and US/Eastern.  This
     // would depend on the browswer's implementatin of the Date
     // object's getTimezoneOffset() method.  US/Eastern is 5 hours
-    // (300 minutes) behind UTC in January, and during daylight
-    // saviing time (DST), 240 minutes.  So for example, if the
-    // computer is configured for US/Pacific time during DST,
-    // getTimezoneOffset() will return 420, therefore 240-420 = -180,
-    // or 3 hours west of EDT.  For the example of "The Tech Guy,"
-    // this would be added to 14 to produce 11, or 11 AM, which is the
-    // hour of the beginning of the show in Petaluma.
+    // (300 minutes) behind UTC in January, and during daylight saving
+    // time (DST), 240 minutes.  So for example, if the computer is
+    // configured for US/Pacific time during DST, getTimezoneOffset()
+    // will return 420, therefore 240-420 = -180, or 3 hours west of
+    // EDT.  For the example of "The Tech Guy," this would be added to
+    // 14 to produce 11, or 11 AM, which is the hour of the beginning
+    // of the show in Petaluma.
 
     var now = new Date();
     var nowOff = now.getTimezoneOffset();
@@ -42,6 +42,8 @@ function get_show_start_hour_offset() {
 }
 
 // symbolic states of the show
+// This acts sort of like an enum in C
+
 const BEFORE_SHOW = 0,
       BUMP_IN = 1, // technically ON_AIR, but while bumper music is playing
       ON_AIR = 2, // talent should be talking
@@ -56,9 +58,20 @@ const BEFORE_SHOW = 0,
 // the next show.  But SHOW_DONE marks the end of the precomputed
 // events table.
 
-function dbg(lvl, msg) {
+function dbg(lvl, ...msg) {
+
+    // This function is called for debugging.  "lvl" describes the
+    // intended level of the message, from lower numbers being very
+    // low level details, like every single member being added to a
+    // complex structure, to higher numbers like general program
+    // state.  If "lvl" is greater than or equal to the global
+    // variable "dbug," the message (including its level and a colon)
+    // goes to the debugging console, otherwise it's just ignored.
+    // There are no constraints on "lvl" or "dbug", for example they
+    // could be negative.
+
     if ( lvl >= dbug ) {
-	console.log(lvl+": "+msg);
+	console.log(lvl+": "+msg.join(" "));
     }
     return true;
 }
@@ -80,14 +93,16 @@ function zeropad(num, places) {
 }
 
 
-var brk;  // the list of breaks
+var brk = [];  // the list of breaks
 
 function load_breaks(profname) {
 
     // Load the breaks list from the localStorage key named
     // "profname" and return it.  Historical note: this used
     // to append "_breaks" to the key name, but then that did
-    // not allow for the idea of a profile backup name.
+    // not allow for the idea of a profile backup name.  If the
+    // "profname" profile is not in localStorage, see if "defaultbrk"
+    // exists and prompt the user to return that instead.
 
     var breaktab = try_load_breaks(profname);
 
@@ -96,8 +111,10 @@ function load_breaks(profname) {
     }
 
     if ( Array.isArray(defaultbrk) ) {
-	console.warn('Tried to load breaks "'+profname+'" but instead returning default array of breaks.');
-	return defaultbrk;
+	console.warn('Tried to load breaks "'+profname+'" but that does not exist.');
+	if ( confirm('Profile "'+profname.replace(/_breaks/, "")+'" not found in localStorage, load the default list instead?') ) {
+	    return defaultbrk;
+	}
     }
 
     return false;
@@ -108,9 +125,11 @@ function try_load_breaks(profname) {
     // This does the actual work of attempting to load the profile.
     // This was refactored because there are now two ways this can
     // fail to load the profile (return false), and I wanted an idea
-    // of a built-in default breaks list.  So the "API" as it were
-    // calls "load_breaks()" and if we return false here, it sees if
-    // there exists a "defaultbrk" array and returns that.
+    // of a built-in default breaks list.  So the "API" as it were is
+    // to call "load_breaks()" and if we return false from
+    // try_load_breaks(), load_breaks() sees if there exists a
+    // "defaultbrk" array and returns that instead.  It's just a lot
+    // handier this way, instead of longer if(){} blocks or a label.
 
     var breaklist = new Array();
     var breakListStr;
@@ -139,8 +158,10 @@ function try_load_breaks(profname) {
 }
 
 function break_cmp(obja, objb) {
+
     // When sorting the brk array (the break descriptions), this
     // provides the comparison function to the .sort() method.
+
     return obja.begin - objb.begin;
 }
 
@@ -174,7 +195,9 @@ function secs2minsec(insecs) {
 
 function secs2hmsStr(insecs) {
 
-    // form an hours, minutes, seconds string from "insecs" number of seconds
+    // form an hours, minutes, seconds string (hh:mm:ss) from "insecs"
+    // number of seconds.  Each portion of the results is zero padded
+    // to 2 digits.
 
     var str = "";
     var t = insecs;
@@ -302,15 +325,32 @@ function st2col(st) {
 
 function find_all_profiles() {
 
+    // finds all the profiles in localStorage and inserts <option>
+    // elements into the node with an ID of "profiles" (expected to be
+    // a <datalist> element)
+
     var i, l, k;
     var seen = new Array();
     var prof;
     var optproto = document.createElement("option");
     var opt;
+    var profblk;
 
     var brksidx, klen;
 
-    // empty any existing options
+    if ( typeof profiles !== "undefined" ) {
+	profblk = profiles;
+    } else {
+	if ( (profblk = document.getElementById("profiles")) === null ) {
+	    // If one isn't in the document already, provide one.
+	    // Other JavaScript could likewise use appendChild to add
+	    // an input field which utilizes this list.
+	    profblk = document.createElement("datalist");
+	    document.body.appendChild(profblk);
+	}
+    }
+
+    // empty out any existing options
     while ( (opt = profiles.firstChild) !== null ) {
 	profiles.removeChild(opt);
     }
@@ -326,7 +366,7 @@ function find_all_profiles() {
 	    seen.push(prof);
 	    opt = optproto.cloneNode(false);
 	    opt.textContent = prof;
-	    profiles.appendChild(opt);
+	    profblk.appendChild(opt);
 	}
     }
 }
